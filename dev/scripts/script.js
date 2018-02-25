9797abdc1e15ae7eb07a1069c7a62ed383503163
lehronn <lehronn@gmail.com>
2018-02-25 17:58:07 +0100

    localscrolling animated, it works

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -1,1 +1,18 @@
+$(document).ready(function () {
+  // start of LOCALSCROLL ANIMATE
+  $("#navbar ul li a[href^='#']").on('click', function(e) {
+    // prevent default anchor click behavior
+    var target = this.hash;
+    e.preventDefault();
 
+    // animate
+    $('html, body').animate({
+      scrollTop: $(this.hash).offset().top
+    }, 1300, function(){
+          // when done, add hash to url
+          // (default click behaviour)
+          window.location.hash = target;
+        });
+  });
+  // end of LOCALSCROLL ANIMATE
+});
